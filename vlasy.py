12f827a1435450a2e376a8d1881ef8b1c2b9f7bc
silvikavcak <78972669+silvikavcak@users.noreply.github.com>
2021-03-05 19:33:16 +0100

    Add files via upload
    Assesses your hair based on number of hairs.

---
+++ vlasy.py
@@ -0,0 +1,11 @@
+vlas= int(input('Ta kolko mas vlasov? '))
+
+if vlas >= 1500000:
+    print("Hrivu Ti zavidi i najvlasatejsia prachovka od nemeckeho Leifheitu!")
+elif vlas >= 180000:
+    print("Vsetky skraslovacie kliniky na nastrelovanie vlasov by nemali co jest, keby boli vsetci ludia taki vlasatci ako Ty.")
+elif vlas >= 1:
+    print("Moj dedo hovoril : Kde je chlp, tam je chut. A Ty chut mas.")
+else:
+    print("I pekna pleska je hitom dneska")
+    
